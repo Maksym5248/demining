792462f5b2f3521d21e865cb5327a4095ab8fbbc
Max Kostin <max.kostin.dev@gmail.com>
2024-01-20 18:42:45 +0200

    feat: caprure image of google maps

--- src/modals/mission-report-create/components/map/map.tsx
+++ src/modals/mission-report-create/components/map/map.tsx
@@ -6,6 +6,25 @@ const mapContainerStyle = {
 };
 
 export function Map(){
+	// const [canvas, setCanvas] = useState<string>();
+	
+	// const onStartCapture = async () => {
+	// 	const url = await Image.takeMapImage("#capture");
+	// 	setCanvas(url)
+	// };
 
-	return <MapView mapContainerStyle={mapContainerStyle}/>
+	// const handleSave = () => {
+	// 	const downloadLink = document.createElement('a');
+	// 	const fileName = 'react-screen-capture.png';
+
+	// 	downloadLink.href = screenCapture;
+	// 	downloadLink.download = fileName;
+	// 	downloadLink.click();
+	// };
+
+	return (
+		<div id="capture">
+			<MapView mapContainerStyle={mapContainerStyle}/>
+		</div>
+	)
 }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
 export * from './storage/storage';
 export * from './storage/secure-storage';
 export * from './modal';
+export * from './image';
