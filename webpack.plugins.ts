0f3365a1e2748a4b7f1521d6578c192442c92393
Max Kostin <max.kostin.dev@gmail.com>
2024-01-28 12:07:17 +0200

    feat: integrate sentry

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,6 +1,7 @@
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin' ;
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin' ;
+import { sentryWebpackPlugin } from '@sentry/webpack-plugin' ;
 
 export const plugins = [
 	new ForkTsCheckerWebpackPlugin(),
@@ -9,4 +10,10 @@ export const plugins = [
 		filename: '[name].[chunkhash].css',
 		chunkFilename: '[name].[chunkhash].chunk.css',
 	}),
+	sentryWebpackPlugin({
+		// SENTRY_AUTH_TOKEN
+		authToken: "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",
+		org: "realty-jo",
+		project: "dsns",
+	  }),
 ].filter(Boolean)
