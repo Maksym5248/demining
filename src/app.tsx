eefecc6e3963dff27b8d243a70ac48608d58f151
Max Kostin <max.kostin.dev@gmail.com>
2024-02-01 16:15:21 +0200

    test

--- src/app.tsx
+++ src/app.tsx
@@ -3,39 +3,39 @@ import { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ConfigProvider } from 'antd';
 
-import { RootStoreContext } from '~/context';
-import { createStore } from '~/stores';
-import { RootRouter, modals } from '~/routes';
-import { ThemeProvider, ModalProvider } from '~/containers';
-import { LogLevel, Logger } from '~/services';
+// import { RootStoreContext } from '~/context';
+// import { createStore } from '~/stores';
+// import { RootRouter, modals } from '~/routes';
+// import { ThemeProvider, ModalProvider } from '~/containers';
+// import { LogLevel, Logger } from '~/services';
 
-import { CONFIG } from './config';
+// import { CONFIG } from './config';
 
 import "./index.css";
 
 
-Logger.setLevel(CONFIG.IS_DEBUG ? LogLevel.Debug : LogLevel.None);
+// Logger.setLevel(CONFIG.IS_DEBUG ? LogLevel.Debug : LogLevel.None);
 
 
-const { store } = createStore();
+// const { store } = createStore();
 
 function App() {
 	useEffect(() => {
-		store.init();
+		// store.init();
 
-		return () => {
-			store.removeAllListeners();
-		}
+		// return () => {
+		// 	store.removeAllListeners();
+		// }
 	}, []);
 
 	return (
 		<ConfigProvider>
-			<ThemeProvider>
+			{/* <ThemeProvider>
 				<RootStoreContext.Provider value={store}>
 					<RootRouter />
 					<ModalProvider modals={modals}/>
 				</RootStoreContext.Provider>
-			</ThemeProvider>
+			</ThemeProvider> */}
 		</ConfigProvider>
 	)
 }
