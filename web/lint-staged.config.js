c97aae1eff530fa2e61df6b8269d76a40fbffa3c
Max Kostin <max.kostin.dev@gmail.com>
2024-05-29 21:50:00 +0300

    feat: create shared ts config

--- web/lint-staged.config.js
+++ web/lint-staged.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    '**/*.(ts|tsx)': () => 'yarn check:tsc',
+	'**/*.(ts|tsx)': () => 'yarn check:tsc',
 
-    '**/*.(ts|tsx|js|jsx)': (filenames) => [
-        `yarn check:eslint -- ${filenames.join(' ')}`,
-    ],
+	'**/*.(ts|tsx|js|jsx)': (filenames) => [
+		`yarn check:eslint -- ${filenames.join(' ')}`,
+	],
 
-    '**/*.(md|json|yml|html)': (filenames) => `yarn check:prettier -- ${filenames.join(' ')}`,
+	'**/*.(md|json|yml|html)': (filenames) => `yarn check:prettier -- ${filenames.join(' ')}`,
 };
