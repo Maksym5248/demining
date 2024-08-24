17c3d94d7da235e84bfb028a4375a459b7f1c625
Max.K <max.kostin.dev@gmail.com>
2024-08-24 10:28:54 +0300

    feat: add background color

--- apps/ammo/src/screens/sign-in/sign-in.style.tsx
+++ apps/ammo/src/screens/sign-in/sign-in.style.tsx
@@ -1,8 +1,9 @@
 import { ThemeManager } from '~/styles';
 
-export const useStyles = ThemeManager.createStyleSheet(() => ({
+export const useStyles = ThemeManager.createStyleSheet(({ theme }) => ({
     container: {
         display: 'flex',
         flex: 1,
+        backgroundColor: theme.colors.background,
     },
 }));
