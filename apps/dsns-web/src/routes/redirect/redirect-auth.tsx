0dce444dcb17ad42c92cb270a242db05ae499028
Max.K <max.kostin.dev@gmail.com>
2024-09-24 21:40:10 +0300

    fix: classefication and uplod jpg image

--- apps/dsns-web/src/routes/redirect/redirect-auth.tsx
+++ apps/dsns-web/src/routes/redirect/redirect-auth.tsx
@@ -9,9 +9,9 @@ export const RedirectAuth = observer(() => {
     const location = useLocation();
     const context = useOutletContext<any>();
 
-    const { isAuthorized } = store.viewer.user ?? {};
+    const { isAuthorizedDSNS } = store.viewer.user ?? {};
 
-    if (!isAuthorized) {
+    if (!isAuthorizedDSNS) {
         return <Navigate to={ROUTES.LOGIN} state={{ from: location }} replace />;
     }
 
