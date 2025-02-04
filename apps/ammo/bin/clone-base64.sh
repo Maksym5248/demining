730a62aaa07216811cf2faf0635e888ae950975a
Maxym Kostin <maxymkostin@CIDTD-L-P-0263.local>
2025-02-04 20:11:54 +0200

    fix: update ci/cd

--- apps/ammo/bin/clone-base64.sh
+++ apps/ammo/bin/clone-base64.sh
@@ -1,5 +1,4 @@
-cat ./android/app/ammo-dev.keystore | base64 > ./clone/ammo-dev.keystore.base64
-cat ./android/app/ammo-prod.keystore | base64 > ./clone/ammo-prod.keystore.base64
+cat ./android/app/ammo.keystore | base64 > ./clone/ammo.keystore.base64
 cat ./android/app/google-services.json | base64 > ./clone/google-services.json.base64
 cat ./android/app/gradle.sign.properties | base64 > ./clone/gradle.sign.properties.keystore.base64
 cat .env | base64 > ./clone/.env.base64
