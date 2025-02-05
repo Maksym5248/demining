b4d829bdfdd07137b0c258eaa5421892018d4a07
Maxym Kostin <maxymkostin@CIDTD-L-P-0263.local>
2025-02-05 18:56:26 +0200

    fix: update analitics

--- apps/ammo/src/services/analytics.ts
+++ apps/ammo/src/services/analytics.ts
@@ -1,3 +1,4 @@
+import analytics from '@react-native-firebase/analytics';
 import { type IAnalyticsEventParams, type IAnalytics, type ILogger, type ICrashlytics } from 'shared-my-client';
 
 export class AnalyticsClass implements IAnalytics {
@@ -13,20 +14,24 @@ export class AnalyticsClass implements IAnalytics {
     page(name: string) {
         this.crashlytics.setTag('PAGE', name);
         this.logger.log(`ANALYTICS - Page: ${name}`);
+        analytics().logScreenView({ screen_name: name });
     }
 
     modal(name: string) {
         this.crashlytics.setTag('MODAL', name);
         this.logger.log(`ANALYTICS - Modal: ${name}`);
+        analytics().logScreenView({ screen_name: name });
     }
 
     event(name: string, params?: IAnalyticsEventParams) {
         this.crashlytics.addBreadcrumb({ type: 'event', message: name, data: params });
         this.logger.log(`ANALYTICS - Event: ${name}`);
+        analytics().logEvent(name, params);
     }
 
     setUserId = (uid: string | null) => {
         this.crashlytics.setUser(uid ? { id: uid } : null);
         this.logger.log(`ANALYTICS: User ID: ${uid}`);
+        analytics().setUserId(uid);
     };
 }
