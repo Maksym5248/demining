8db64a9d8c3896852b0a2d1a1e64f809a30af1d7
Maxym Kostin <maxymkostin@CIDTD-L-P-0263.local>
2025-02-06 13:08:39 +0200

    feat: setup settings

--- apps/ammo/bin/clone-base64.sh
+++ apps/ammo/bin/clone-base64.sh
@@ -1,5 +1,14 @@
-cat ./android/app/ammo.keystore | base64 > ./clone/ammo.keystore.base64
-cat ./android/app/google-services.json | base64 > ./clone/google-services.json.base64
-cat ./android/app/gradle.sign.properties | base64 > ./clone/gradle.sign.properties.keystore.base64
-cat ./android/sentry.properties | base64 > ./clone/sentry.properties.base64
-cat .env | base64 > ./clone/.env.base64
+if [ ! -d ./config ]; then
+    mkdir ./config
+fi
+
+if [ ! -d ./config/clone ]; then
+    mkdir ./config/clone
+fi
+
+cat ./config/ammo.keystore | base64 > ./config/clone/ammo.keystore.base64
+cat ./config/google-services.json | base64 > ./config/clone/google-services.json.base64
+cat ./config/GoogleService-Info.plist > ./config/clone/GoogleService-Info.plist
+cat ./config/gradle.sign.properties | base64 > ./config/clone/gradle.sign.properties.keystore.base64
+cat ./config/sentry.properties | base64 > ./config/clone/sentry.properties.base64
+cat .env | base64 > ./config/clone/.env.base64
