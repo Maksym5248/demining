e0f8cd0d62130a4ada1614060e04450241ca8953
Maxym Kostin <maxymkostin@CIDTD-L-P-0263.local>
2025-02-06 13:40:42 +0200

    feat: integrate fastlane

--- apps/ammo/bin/clone-base64.sh
+++ apps/ammo/bin/clone-base64.sh
@@ -9,6 +9,6 @@ fi
 cat ./config/ammo.keystore | base64 > ./config/clone/ammo.keystore.base64
 cat ./config/google-services.json | base64 > ./config/clone/google-services.json.base64
 cat ./config/GoogleService-Info.plist > ./config/clone/GoogleService-Info.plist
-cat ./config/gradle.sign.properties | base64 > ./config/clone/gradle.sign.properties.keystore.base64
+cat ./config/gradle.sign.properties | base64 > ./config/clone/gradle.sign.properties.base64
 cat ./config/sentry.properties | base64 > ./config/clone/sentry.properties.base64
 cat .env | base64 > ./config/clone/.env.base64
