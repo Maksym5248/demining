dae93599156b3c7632bac814b1e1b748bf092991
Maxym Kostin <maxymkostin@CIDTD-L-P-0263.local>
2025-02-27 00:37:24 +0200

    feat: add comments for pdf reader

--- apps/ammo/src/screens/reader/reader.screen.tsx
+++ apps/ammo/src/screens/reader/reader.screen.tsx
@@ -10,6 +10,21 @@ import { useStylesCommon, useTheme } from '~/styles';
 import { useStyles } from './reader.style';
 import { type IReaderScreenProps } from './reader.types';
 
+/**
+ * 1 - horizontal and vertiacal scroll
+ * 2 - show total pages and current page
+ * 3 - search and highlight text
+    <AppStack.Screen
+    name="VehiclesDetailsAuction"
+    component={VehiclesDetailsAuction}
+    options={{ orientation: 'portrait' }}
+    />
+    <AppStack.Screen
+    name="VehicleImageView"
+    component={VehicleImageView}
+    options={{ orientation: 'all' }}
+/>
+*/
 export const ReaderScreen = observer(({ route }: IReaderScreenProps) => {
     const { uri, title } = route?.params || {};
     const theme = useTheme();
