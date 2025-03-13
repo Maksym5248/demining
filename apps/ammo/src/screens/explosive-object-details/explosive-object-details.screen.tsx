d86c2b379df8e5a3303b3fe752d78d858a39c0ef
Maxym Kostin <maxymkostin@CIDTD-L-P-0263.local>
2025-03-13 15:36:58 +0200

    feat: add efforts to sensivity

--- apps/ammo/src/screens/explosive-object-details/explosive-object-details.screen.tsx
+++ apps/ammo/src/screens/explosive-object-details/explosive-object-details.screen.tsx
@@ -63,6 +63,7 @@ export const ExplosiveObjectDetailsScreen = observer(({ route }: IExplosiveObjec
                         />
                     )}
                     <Field.View label={t('targetSensor')} text={details?.data.targetSensor} require={false} />
+                    <Field.Range label={t('sensitivityEffort')} value={details?.data.sensitivity?.effort} require={false} />
                     <Field.View label={t('sensitivity')} text={details?.data.sensitivity?.sensitivity} require={false} />
                     {details?.data.sensitivity?.additional?.map(el => (
                         <Field.View key={el.name} label={el.name} text={el.value} require={false} />
@@ -70,7 +71,12 @@ export const ExplosiveObjectDetailsScreen = observer(({ route }: IExplosiveObjec
                     <Field.View label={t('liquidatorShort')} text={details?.data.liquidatorShort} require={false} />
                     <Field.View label={t('foldingShort')} text={details?.data.foldingShort} require={false} />
                     <Field.View label={t('extractionShort')} text={details?.data.extractionShort} require={false} />
-                    <Field.Range label={t('demageRadius')} value={details?.data.damage?.radius} require={false} />
+                    <Field.Range
+                        label={t('demageRadius')}
+                        value={details?.data.damage?.radius}
+                        info={t('demageRadiusInfo')}
+                        require={false}
+                    />
                     <Field.Range label={t('demageDistance')} value={details?.data.damage?.distance} require={false} />
                     <Field.Range label={t('demageSquad')} value={details?.data.damage?.squad} require={false} />
                     <Field.Range label={t('demageHeight')} value={details?.data.damage?.height} require={false} />
