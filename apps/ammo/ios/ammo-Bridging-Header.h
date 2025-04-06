cb0df1f139cd155381322f12edeba42a974b430c
Maxym Kostin <maxymkostin@CIDTD-L-P-0263.local>
2025-04-06 14:57:41 +0300

    feat: update ios deps

---
+++ apps/ammo/ios/ammo-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  ammo-Bridging-Header.h
+//  ammo
+//
+//  Created by Maxym Kostin on 06.04.2025.
+//
+
+#ifndef ammo_Bridging_Header_h
+#define ammo_Bridging_Header_h
+
+
+#endif /* ammo_Bridging_Header_h */
