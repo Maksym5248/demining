fafbec9a824ac0606367144d7cd3a016f07f68fa
maxymkostin <max.kostin.dev@gmail.com>
2025-04-20 08:11:39 +0300

    feat: update react for web

--- apps/dsns-web/src/index.tsx
+++ apps/dsns-web/src/index.tsx
@@ -1,6 +1,19 @@
+import * as Sentry from '@sentry/react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './app';
 
-const root = createRoot(document.getElementById('root') as HTMLElement);
+const container = document.getElementById('root') as HTMLElement;
+
+const root = createRoot(container, {
+    // Callback called when an error is thrown and not caught by an ErrorBoundary.
+    onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+        console.warn('Uncaught error', error, errorInfo.componentStack);
+    }),
+    // Callback called when React catches an error in an ErrorBoundary.
+    onCaughtError: Sentry.reactErrorHandler(),
+    // Callback called when React automatically recovers from errors.
+    onRecoverableError: Sentry.reactErrorHandler(),
+});
+
 root.render(<App />);
--- apps/dsns-web/src/services/crashlytics.ts
+++ apps/dsns-web/src/services/crashlytics.ts
@@ -12,13 +12,7 @@ export class CrashlyticsClass implements ICrashlytics {
             Sentry.init({
                 dsn: CONFIG.SENTRY,
                 environment: process.env.ENV,
-                integrations: [
-                    new Sentry.BrowserTracing(),
-                    new Sentry.Replay({
-                        maskAllText: false,
-                        blockAllMedia: false,
-                    }),
-                ],
+                integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
                 tracesSampleRate: 1.0, //  Capture 100% of the transactions
                 replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
                 replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
