4a626ff7050cfbb12dcabe55010dc63eb2e0b0b8
maxymkostin <max.kostin.dev@gmail.com>
2025-04-30 16:43:11 +0300

    feat: update translation

--- apps/ammo/src/services/permissions/permissions-list.ts
+++ apps/ammo/src/services/permissions/permissions-list.ts
@@ -9,8 +9,8 @@ import { type IListPermissions } from '../../types';
 export const listPermissions: IListPermissions = {
     [PERMISSIONS.PHOTO_LIBRARY]: {
         name: Device.isIOS ? PERM_LIB.IOS.PHOTO_LIBRARY : PERM_LIB.ANDROID.READ_MEDIA_IMAGES,
-        title: t('permissions.cameraTitle'),
-        message: t('permissions.cameraMessage'),
+        title: t('permissions.photoLibaryTitle'),
+        message: t('permissions.photoLibaryMessage'),
     },
     [PERMISSIONS.NOTIFICATIONS]: {
         name: Device.isIOS ? PERM_LIB.IOS.APP_TRACKING_TRANSPARENCY : null,
