ba699d818c5a65e94d74c405f31758b7ec0a62aa
maxymkostin <max.kostin.dev@gmail.com>
2025-06-09 21:46:08 +0300

    feat: update build script

--- apps/functions/bin/deploy-v2.sh
+++ apps/functions/bin/deploy-v2.sh
@@ -3,13 +3,18 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Load environment variables from .env file
+if [ -f "../.env" ]; then
+  export $(cat ../.env | xargs)
+fi
+
 # Variables
 IMAGE_NAME="parsebook-container" # Name for your specific container image
 IMAGE_TAG="local-$(date +%Y%m%d%H%M%S)" # Use timestamp as tag for uniqueness
-ARTIFACT_REGISTRY_REPO="gcf-artifacts" # Replace with your Artifact Registry repository name
-REGION="europe-central2" # Replace with your desired region
-PROJECT_ID="dsns-dev-85963" # Replace with your GCP project ID
-IMAGE_PATH="${REGION}-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/${IMAGE_NAME}:${IMAGE_TAG}"
+# ARTIFACT_REGISTRY_REPO="gcf-artifacts" # Replace with your Artifact Registry repository name
+# REGION="europe-central2" # Replace with your desired region
+# PROJECT_ID="dsns-dev-85963" # Replace with your GCP project ID
+IMAGE_PATH="${REGION}-docker.pkg.dev/${PROJECT_NAME}/${ARTIFACT_REGISTRY_REPO}/${IMAGE_NAME}:${IMAGE_TAG}"
 
 # Ensure the isolate directory exists
 if [ ! -d "./isolate" ]; then
