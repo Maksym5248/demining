6e69a06e4864cf1e8a04121b49720725b41b7ca2
maxymkostin <max.kostin.dev@gmail.com>
2025-06-09 22:45:12 +0300

    fix: build image

--- apps/functions/bin/build-image-v2.sh
+++ apps/functions/bin/build-image-v2.sh
@@ -3,13 +3,21 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+ENV_FILE=".env"
+if [ ! -f "$ENV_FILE" ]; then
+  echo ".env file not found!"
+  exit 1
+fi
+
+# Load environment variables
+set -a
+source "$ENV_FILE"
+set +a
+
 # Variables
 IMAGE_NAME="parsebook-container" # Name for your specific container image
 IMAGE_TAG="local-$(date +%Y%m%d%H%M%S)" # Use timestamp as tag for uniqueness
-ARTIFACT_REGISTRY_REPO="gcf-artifacts" # Replace with your Artifact Registry repository name
-REGION="us-central1" # Replace with your desired region
-PROJECT_ID="dsns-dev-85963" # Replace with your GCP project ID
-IMAGE_PATH="${REGION}-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/${IMAGE_NAME}:${IMAGE_TAG}"
+IMAGE_PATH="${REGION}-docker.pkg.dev/${PROJECT_NAME}/${ARTIFACT_REGISTRY_REPO}/${IMAGE_NAME}:${IMAGE_TAG}"
 
 # Build and push Docker image
 echo "Building and pushing container image for parseBook to: ${IMAGE_PATH}"
