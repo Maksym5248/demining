27450d3c9eb5c3cfeee5df252885f631d17e49eb
maxymkostin <max.kostin.dev@gmail.com>
2025-06-09 21:13:16 +0300

    feat: create artifact registry repo

--- apps/functions/bin/build-v2.sh
+++ apps/functions/bin/build-v2.sh
@@ -11,6 +11,15 @@ REGION="europe-central2" # Replace with your desired region
 PROJECT_ID="dsns-dev-85963" # Replace with your GCP project ID
 IMAGE_PATH="${REGION}-docker.pkg.dev/${PROJECT_ID}/${ARTIFACT_REGISTRY_REPO}/${IMAGE_NAME}:${IMAGE_TAG}"
 
+# Ensure the Artifact Registry repository exists
+if ! gcloud artifacts repositories describe "${ARTIFACT_REGISTRY_REPO}" --location="${REGION}" --project="${PROJECT_ID}" > /dev/null 2>&1; then
+  echo "Artifact Registry repository '${ARTIFACT_REGISTRY_REPO}' does not exist. Creating it..."
+  gcloud artifacts repositories create "${ARTIFACT_REGISTRY_REPO}" \
+    --repository-format=docker \
+    --location="${REGION}" \
+    --description="Repository for parseBook container images"
+fi
+
 # Ensure the isolate directory exists
 if [ ! -d "./isolate" ]; then
   echo "Error: isolate directory not found. Ensure yarn isolate is run successfully."
@@ -26,3 +35,9 @@ cd ./isolate
 # Build and push Docker image
 echo "Building and pushing container image for parseBook to: ${IMAGE_PATH}"
 gcloud builds submit . --tag "${IMAGE_PATH}"
+
+# TODO:
+#  - avoid publish image every push just do it when Dockerfile changes
+#  - remove artifacts what are not using anymore
+#  - autoremove old artifacts
+
