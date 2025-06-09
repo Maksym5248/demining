28524575be2c671a166a5c3aba0f7f9026cdf0d9
maxymkostin <max.kostin.dev@gmail.com>
2025-06-09 23:03:37 +0300

    feat: restore build image

--- apps/functions/bin/build-image-v2.sh
+++ apps/functions/bin/build-image-v2.sh
@@ -15,15 +15,34 @@ source "$ENV_FILE"
 set +a
 
 # Variables
-IMAGE_NAME="parsebook-container" # Name for your specific container image
-IMAGE_TAG="local-$(date +%Y%m%d%H%M%S)" # Use timestamp as tag for uniqueness
+IMAGE_NAME="parsebook-container"
+IMAGE_TAG="local-$(date +%Y%m%d%H%M%S)"
 IMAGE_PATH="${REGION}-docker.pkg.dev/${PROJECT_NAME}/${ARTIFACT_REGISTRY_REPO}/${IMAGE_NAME}:${IMAGE_TAG}"
+
+# Ensure the isolate directory exists
+if [ ! -d "./isolate" ]; then
+  echo "Error: isolate directory not found. Ensure yarn isolate is run successfully."
+  exit 1
+fi
+
+# Copy Dockerfile into the isolate directory
+cp ./Dockerfile ./isolate/
+
+# Change to the isolate directory
+cd ./isolate
 
 # Build and push Docker image
 echo "Building and pushing container image for parseBook to: ${IMAGE_PATH}"
 
-# Use the current directory as the source
+# Use a custom logs bucket to avoid streaming issues
 gcloud builds submit . --tag "${IMAGE_PATH}"
 
 echo "Artifact successfully deployed. Exiting..."
 exit 0
+
+  
+# TODO:
+#  - avoid publish image every push just do it when Dockerfile changes
+#  - remove artifacts what are not using anymore
+#  - autoremove old artifacts
+
