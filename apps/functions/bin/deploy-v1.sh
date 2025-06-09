a5f2b91a8928a36e0d468956abf40df0815d5386
maxymkostin <max.kostin.dev@gmail.com>
2025-06-10 00:57:15 +0300

    fix syntax

--- apps/functions/bin/deploy-v1.sh
+++ apps/functions/bin/deploy-v1.sh
@@ -15,7 +15,7 @@ source "$ENV_FILE"
 set +a
 
 # Deploy all functions
-firebase deploy --project "${PROJECT_NAME}" --region="${REGION}" --only functions:onMemberUpdate,functions:onUserAccessUpdate,functions:onUserCreate,functions:translateOnWrite,functions:onUserDelete,functions:onCommentReplyWrite
+firebase deploy --project "${PROJECT_NAME}" --region "${REGION}" --only functions:onMemberUpdate,functions:onUserAccessUpdate,functions:onUserCreate,functions:translateOnWrite,functions:onUserDelete,functions:onCommentReplyWrite
 
 echo "Cloud Function successfully deployed. Exiting..."
 exit 0
